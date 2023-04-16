867be526257045bb4e08d88ec61348b788d91e0a
Aditya Salunke <adisalu15@gmail.com>
2023-04-17 04:25:33 +0530

    added alert and validation

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertController, IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,ReactiveFormsModule]
 })
 export class Tab4Page implements OnInit {
+  feedbackForm!: FormGroup;
 
-  constructor() { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Feedback',
+      message: 'Thank you for your feedback',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+  constructor(public alertController: AlertController,private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.feedbackForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      rating: [5, Validators.required],
+      suggestion: ['', Validators.required]
+    });
   }
 
 }
